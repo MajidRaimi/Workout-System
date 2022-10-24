173d27785cf645723a099e9e5cb1e83ee505272d
MajidRaimi <imajid2000z1@gmail.com>
2022-10-24 20:52:29 +0300

    created react app

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import {Link} from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <header>
+            <div className = 'container' >
+                <Link to = '/'>
+                    <h4>Workout Buddy</h4>
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar;
